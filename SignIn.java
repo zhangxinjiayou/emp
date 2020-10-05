ac2825771b58b72cc8926bb895c63e34fa3745d1
zhangixin <zhangixin@etoak.com>
2020-10-05 22:22:11 +0800

    在家开发其他的签到功能

---
+++ SignIn.java
@@ -0,0 +1,11 @@
+/*
+* @Author: Marte
+* @Date:   2020-10-05 21:59:06
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-10-05 22:20:15
+*/
+
+public class SignIn {
+    private String name;
+    private String sign_time;
+}
