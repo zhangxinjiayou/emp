ec4a9de9747eda122944a5bfb553902677a0c768
zhangxin <zhangxin@etoak.com>
2020-10-05 22:17:01 +0800

    在公司开发部分签到功能

---
+++ SignIn.java
@@ -0,0 +1,11 @@
+/*
+* @Author: Marte
+* @Date:   2020-10-05 21:50:17
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-10-05 22:16:40
+*/
+
+public class SignIn {
+   private int id;
+   private String name;
+}
